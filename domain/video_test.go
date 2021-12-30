283f4de622b68f159dc19c3c080dc11ffb9d74e7
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2021-12-30 18:57:48 -0300

    testing video

--- domain/video_test.go
+++ domain/video_test.go
@@ -1,8 +1,10 @@
 package domain_test
 
 import (
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 	"video-encoder/domain"
 )
 
@@ -11,4 +13,30 @@ func TestValidateIfVideoIsEmpty(t *testing.T) {
 	err := video.Validate()
 
 	require.Error(t, err)
+}
+
+func TestVideoIdIsNotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoValidationt(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Nil(t, err)
 }
